f820535bb515b312c564e1f2220d374c844091ea
Louis Marchal <louis.marchal05@gmail.com>
2021-09-03 19:24:28 +0200

    init repo

---
+++ target/debug/deps/digify-57d6289253dfe090.d
@@ -0,0 +1,5 @@
+/home/louis/Bureau/dev/digify/target/debug/deps/digify-57d6289253dfe090.rmeta: src/main.rs
+
+/home/louis/Bureau/dev/digify/target/debug/deps/digify-57d6289253dfe090.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/digify-8bdfbf311d96dd05.d
@@ -0,0 +1,5 @@
+/home/louis/Bureau/dev/digify/target/debug/deps/digify-8bdfbf311d96dd05.rmeta: src/main.rs
+
+/home/louis/Bureau/dev/digify/target/debug/deps/digify-8bdfbf311d96dd05.d: src/main.rs
+
+src/main.rs:
