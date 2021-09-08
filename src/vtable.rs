553c36a19eee968525851f570a4931fdbec20891
Louis Marchal <louis.marchal05@gmail.com>
2021-09-08 21:56:10 +0200

    fix flatten, from trait impl and match method

--- src/vtable.rs
+++ src/vtable.rs
@@ -43,54 +43,13 @@ impl Name {
 
 pub enum RawDimension<'di> {
     Unite(String),
-    Power(&'di RawDimension<'di>,Sign, u32),
+    Power(&'di RawDimension<'di>,Sign, i32),
     Composit(&'di RawDimension<'di>, Operator, &'di RawDimension<'di>),
 
 }
-impl<'di> From<RawDimension<'di>> for Dimension<'di>{
-    fn from(dim: RawDimension<'di>) -> Dimension<'di>{
-        match self{
-            Self::Unite(name) => {
-                Dimension{
-                    flattened: vec![*self],
-                    tree: *self
-                }
-            }
-            Self::Power(dim,sign, pow) => {
-                let resolved = Self::Power(*dim, sign.resolve(power));
-                Dimension{
-                    flattened: vec![*self],
-                    tree: *self
-                }
-            }
-            Self::Composit(dim1, op, dim2) => {
-                match op{
-                    Operator::Mul => {
-                        Dimension::combine(dim1.parse(sign), *op, dim2.parse(sign))
-                    },
-                    Operator::Div => {
-                        Dimension::combine(dim1.parse(sign), *op, dim2.parse(sign.flip()))
-                    }
-                }
-            }
-        }
-    }
-}
-
 struct Dimension<'di>{
     flattened: Vec<RawDimension<'di>>,
     tree: RawDimension<'di>
-}
-impl<'a> Dimension<'a>{
-    fn combine(dim1: Dimension<'a>, op: Operator, dim2: Dimension<'a>) -> Dimension<'a>{
-        let flattened = vec![dim1.flattened, dim2.flattened].into_iter().flatten().collect::<Vec<RawDimension<'a>>>();
-        let tree = RawDimension::Composit(&dim1.tree, op, &dim2.tree);
-        Dimension{
-            flattened: flattened,
-            tree:tree
-        }
-
-    }
 }
 
 pub enum Operator {
@@ -113,6 +72,85 @@ impl Sign {
         match self {
             Self::Plus => power,
             Self::Minus => -power
+        }
+    }
+    const fn add(&self, other: Self) -> Self {
+        match self{
+            Self::Plus => {
+                match other {
+                    Self::Plus => Self::Plus,
+                    Self::Minus => Self::Minus
+                }
+            },
+            Self::Minus => {
+                match other{
+                    Self::Plus => Self::Minus,
+                    Self::Minus => Self::Plus
+                }
+            }
+        }
+    }
+}
+
+impl<'di> From<RawDimension<'di>> for Dimension<'di>{
+    fn from(dim: RawDimension<'di>) -> Dimension<'di>{
+        Dimension{
+            flattened: dim.flatten_recursive(Sign::Plus, 1),
+            tree: dim
+        }
+        }
+    }
+
+impl<'a> RawDimension<'a>{
+    fn flatten_recursive(&'a self, sign: Sign, power: i32) -> Vec<RawDimension>{
+        match self{
+            Self::Unite(name) => {
+                vec![Self::Power(&RawDimension::Unite(name.to_string()), Sign::Plus, sign.resolve(power))]
+            }
+            Self::Power(dim,sign_pow, pow) => {
+                dim.flatten_recursive(sign_pow.add(sign), power*pow)
+            }
+            Self::Composit(dim1, op, dim2) => {
+                match op{
+                    Operator::Mul => {
+                        let array = vec![dim1.flatten_recursive(sign, power), dim2.flatten_recursive(sign, power)];
+                        array.into_iter().flatten().collect::<Vec<RawDimension>>()
+                    },
+                    Operator::Div => {
+                        vec![dim1.flatten_recursive(sign, power), dim2.flatten_recursive(sign.flip(), power)].into_iter().flatten().collect::<Vec<RawDimension>>()
+                    }
+                }
+            }
+        }   
+    }
+}
+impl<'a> Dimension<'a>{
+    fn check(&'a self, other: Self) -> Option<bool>{
+        self.flattened.into_iter().fold(Some(true), |before, element|{
+            let r = if let RawDimension::Power(unit, _,pow) = element{
+                if let RawDimension::Unite(own_name) = unit{
+                    other.flattened.into_iter().flat_map(|el| {
+                        if let RawDimension::Power(unit_other,_, pow_other ) = el{
+                            if let RawDimension::Unite(uname) = unit_other{
+                                if (own_name == uname) && (pow == pow_other){
+                                    Some(true)
+                                }else{
+                                    None
+                                }
+                            }else{
+                                None
+                            }
+                        }else{
+                            None
+                        }
+                    }).collect::<Vec<bool>>().first()
+                } else {
+                    None
+                }
+            } else {
+                None
+            };
+            Some(*r? && before?)
         }
     }
 }
