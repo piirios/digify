d0d521427702c8a8f566ebe5d12680de69f6d20f
Tatounee <oettam32@gmail.com>
2021-09-09 10:15:09 +0200

    remove target folder

--- target/debug/deps/digify-57d6289253dfe090.d
+++
@@ -1,5 +0,0 @@
-/home/louis/Bureau/dev/digify/target/debug/deps/digify-57d6289253dfe090.rmeta: src/main.rs
-
-/home/louis/Bureau/dev/digify/target/debug/deps/digify-57d6289253dfe090.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/digify-8bdfbf311d96dd05.d
+++
@@ -1,5 +0,0 @@
-/home/louis/Bureau/dev/digify/target/debug/deps/digify-8bdfbf311d96dd05.rmeta: src/main.rs
-
-/home/louis/Bureau/dev/digify/target/debug/deps/digify-8bdfbf311d96dd05.d: src/main.rs
-
-src/main.rs:
